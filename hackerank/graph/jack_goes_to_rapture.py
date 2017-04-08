ddc6dd2329e444e232df3512c1272dd51924a33f
vikash dahiya <vikashdahiya947@outlook.com>
2017-04-09 01:06:38 +0530

    [javascript] : patterns and conscepts

--- hackerank/graph/jack_goes_to_rapture.py
+++ hackerank/graph/jack_goes_to_rapture.py
@@ -1,20 +1,26 @@
+from collections import deque
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def calculate(n):
+    graph[0]['d'] = 0
+    count = float('inf')
+    for adj in graph[0]['link']:
+        ans = dfs_visit(adj,0,n)
+        if(count > ans):
+            count = ans
+    print count
+    
+def dfs_visit(child,sum,n):
+    v,w = [child['v'],child['w']]
+    if(sum < w):
+        sum = w
+    
+    if(v == n):
+        return sum
+    
+    for adj in graph[v - 1]['link']:
+            sum = dfs_visit(adj,sum,n)
+    
+    return sum
 
 
 
@@ -25,11 +31,12 @@ for i in range(0,N):
     graph_obj = {}
     graph_obj['link'] = []
     graph_obj['d'] =  float('inf')
-for i in range(0,snake):
-    a,b = raw_input().strip().split(' ')
-    graph[a]['sv'] =  b
-ans = calculate()
-if ans == float('inf'):
-    print -1,"****"
-else:
-    print ans,"*********"
+    graph_obj['c'] =  'w'
+    graph.append(graph_obj)
+for i in range(0,M):
+    a,b,r = raw_input().strip().split(' ')
+    a,b,r = [int(a), int(b), int(r)]
+    graph[int(a) - 1]['link'].append({'v' : int(b), 'w' : int(r)})
+    # graph[int(b) - 1]['link'].append({'v' : int(a), 'w' : int(r)})
+
+calculate(N)
